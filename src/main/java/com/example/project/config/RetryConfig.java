a97063e7f9ac4e86179209d757594af6db31749c
icebear0111 <wltjq0901@naver.com>
2023-11-29 00:14:22 +0900

    spring retry implement and test

---
+++ src/main/java/com/example/project/config/RetryConfig.java
@@ -0,0 +1,11 @@
+package com.example.project.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.annotation.EnableRetry;
+
+@EnableRetry
+@Configuration
+public class RetryConfig {
+
+}
